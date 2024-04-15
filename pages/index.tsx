3d19e38a91d9ce6c37eadd06ed17644bc70641b2
Bryce Tuppurainen <63719859+BryceTuppurainen@users.noreply.github.com>
2024-04-15 18:56:44 +1000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,12 +12,11 @@ export default function Home() {
   return (
     <>
       <CustomHead />
-
       <main className={styles.main}>
         <header className={styles.header}>
           <h3>tuppurainen.com.au - bryce.tuppurainen.au@gmail.com</h3>
           <h1>Bryce Tuppurainen</h1>
-          <p>A portfolio site by Bryce Tuppurainen developed using <A href="https://nextjs.org/">NextJS</A>, <A href="https://reactjs.org/">ReactJS</A>, <A href="https://sass-lang.com/">Sass</A>, and deployed using <A href="https://www.netlify.com/">Netlify</A>. Written in <A href="https://www.typescriptlang.org/">TypeScript</A>.</p>
+          <p>A portfolio site by Bryce Tuppurainen. Stack: <A href="https://nextjs.org/">NextJS</A>, <A href="https://reactjs.org/">ReactJS</A>, <A href="https://sass-lang.com/">Sass</A>, and deployed using <A href="https://www.netlify.com/">Netlify</A>. Written in <A href="https://www.typescriptlang.org/">TypeScript</A>.</p>
           <div>
             <A href="https://nextjs.org/" target="_blank">
               <Image src="/images/next-logo.webp" alt="NextJS Logo" height={100} width={100} />
@@ -35,25 +34,11 @@ export default function Home() {
               <Image src="/images/typescript-logo.webp" alt="TypeScript Logo" height={100} width={100} />
             </A>
           </div>
-          <p>Click the buttons above to learn more, links on this page are bold</p>
         </header>
-
-
-        <p>This site was published in March 2022. I was in my third year of Software Engineering at the University of Newcastle, and had developed a keen interest in all things full-stack development. Particularly anything written in TypeScript. I was teaching myself various NodeJS Packages and JS/CSS Frameworks, and developing then deploying various sites in my free time for fun and practice including this one. For my more recent endeavours see below...</p>
-
-        <code className={styles.code}><span className={styles.green}>bryce@tuppurainen.com.au ~ $</span> whoami --scroll-down</code>
-
-
         <div className={styles.projects}>
-          <h3>Projects</h3>
-          <ul className={styles.ul}>
-            <li><A href="https://github.com/BryceTuppurainen/">GitHub</A></li>
-            <li>
-              <A href="/standards">Standards Encyclopedia</A></li>
-          </ul>
+          <h3><A href="https://github.com/BryceTuppurainen/">GitHub</A></h3>
+          <p>This site was published in March 2022 in my third year of Software Engineering at the University of Newcastle</p>
         </div>
-
-
       </main>
     </>
   )
